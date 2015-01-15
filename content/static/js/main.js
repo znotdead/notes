04f53df11c96aea13368853c7b6aa83073e8ac05
znotdead <zhirafchik@gmail.com>
2015-01-15 16:44:20 +0800

    image display smaller. show full size on hover

--- content/static/js/main.js
+++ content/static/js/main.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
     $('table').addClass('table');
     $('table').addClass('table-striped');
     $('table').tablesorter();
+    $($('div.container')[1]).find('img').addClass('img-thumbnail');
+    $($('div.container')[1]).find('img').addClass('thumbnail');
+
+    $('.thumbnail').hover(function(){
+        $(this).addClass('fullsizeimage');
+        $(this).removeClass('thumbnail');
+    },
+    function(){
+        $(this).addClass('thumbnail');
+        $(this).removeClass('fullsizeimage');
+    });
 });

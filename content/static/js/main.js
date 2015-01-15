dc6714737d31690bb008b36efd4c864b7bddd322
znotdead <zhirafchik@gmail.com>
2015-01-15 17:23:28 +0800

    fix image #21

--- content/static/js/main.js
+++ content/static/js/main.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
     $('table').addClass('table');
     $('table').addClass('table-striped');
     $('table').tablesorter();
+    // for bootstrap css (display borders)
     $($('div.container')[1]).find('img').addClass('img-thumbnail');
-    $($('div.container')[1]).find('img').addClass('thumbnail');
 
+    // hover
+    $($('div.container')[1]).find('img').addClass('thumbnail');
     $('.thumbnail').hover(function(){
         $(this).addClass('fullsizeimage');
         $(this).removeClass('thumbnail');
